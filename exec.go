fade4894b46fbd605d46589d03b9285fad372ad7
Jason Wilder <mail@jasonwilder.com>
2015-10-27 15:48:59 -0600

    Fix timeout when handling signals

--- exec.go
+++ exec.go
@@ -25,19 +25,18 @@ func runCmd(ctx context.Context, cancel context.CancelFunc, cmd string, args ...
 		log.Fatalf("Error starting command: `%s` - %s\n", cmd, err)
 	}
 
+	// Setup signaling
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL)
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		// Setup signaling
-		sigs := make(chan os.Signal)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGQUIT)
-
 		select {
 		case sig := <-sigs:
 			log.Printf("Received signal: %s\n", sig)
-			ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-			signalProcessWithTimeout(ctx, process, sig)
+			signalProcessWithTimeout(process, sig)
 			cancel()
 		case <-ctx.Done():
 			// exit when context is done
@@ -57,11 +56,18 @@ func runCmd(ctx context.Context, cancel context.CancelFunc, cmd string, args ...
 
 }
 
-func signalProcessWithTimeout(ctx context.Context, process *exec.Cmd, sig os.Signal) {
-	process.Process.Signal(sig) // pretty sure this doesn't do anything. It seems like the signal is automatically sent to the command?
-	process.Wait()
+func signalProcessWithTimeout(process *exec.Cmd, sig os.Signal) {
+	done := make(chan struct{})
+
+	go func() {
+		process.Process.Signal(sig) // pretty sure this doesn't do anything. It seems like the signal is automatically sent to the command?
+		process.Wait()
+		close(done)
+	}()
 	select {
-	case <-ctx.Done():
+	case <-done:
+		return
+	case <-time.After(10 * time.Second):
 		log.Println("Killing command due to timeout.")
 		process.Process.Kill()
 	}
