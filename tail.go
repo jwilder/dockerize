0400d2c630b27acf8dc821e7c87233ff3fc176d8
Brice Figureau <brice@daysofwonder.com>
2017-12-14 14:05:57 +0100

    Fix #100 allowing to tail named pipes
    `hpcloud/tail` supports tailing named pipe if we tell it to do so.
    This patch detects if the tailed file is a named pipe and configure
    `hpcloud/tail` accordingly.
    
    Note that it probably works only on Unix.
    
    Signed-off-by: Brice Figureau <brice@daysofwonder.com>

--- tail.go
+++ tail.go
@@ -11,11 +11,18 @@ import (
 
 func tailFile(ctx context.Context, file string, poll bool, dest *os.File) {
 	defer wg.Done()
+
+	s, err := os.Stat(file)
+	if err != nil {
+		log.Fatalf("unable to stat %s: %s", file, err)
+	}
+
 	t, err := tail.TailFile(file, tail.Config{
 		Follow: true,
 		ReOpen: true,
 		Poll:   poll,
 		Logger: tail.DiscardingLogger,
+		Pipe:   s.Mode()&os.ModeNamedPipe != 0,
 	})
 	if err != nil {
 		log.Fatalf("unable to tail %s: %s", "foo", err)
