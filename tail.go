4c86ed7fc174282230a92f151c65e69ee4704bf3
Jason Wilder <mail@jasonwilder.com>
2018-03-17 14:45:42 -0600

    Fix 100% cpu util when tailing file
    If the file could not be read or tail had an error, the process
    would spin in a look pegging a CPU.

--- tail.go
+++ tail.go
@@ -18,22 +18,31 @@ func tailFile(ctx context.Context, file string, poll bool, dest *os.File) {
 		Logger: tail.DiscardingLogger,
 	})
 	if err != nil {
-		log.Fatalf("unable to tail %s: %s", "foo", err)
+		log.Fatalf("unable to tail %s: %s", file, err)
 	}
+
+	defer func() {
+		t.Stop()
+		t.Cleanup()
+	}()
 
 	// main loop
 	for {
 		select {
 		// if the channel is done, then exit the loop
 		case <-ctx.Done():
-			t.Stop()
-			t.Cleanup()
 			return
 		// get the next log line and echo it out
 		case line := <-t.Lines:
-			if line != nil {
-				fmt.Fprintln(dest, line.Text)
+			if line == nil {
+				if t.Err() != nil {
+					log.Fatalf("unable to tail %s: %s", file, t.Err())
+				}
+				return
+			} else if line.Err != nil {
+				log.Fatalf("unable to tail %s: %s", file, t.Err())
 			}
+			fmt.Fprintln(dest, line.Text)
 		}
 	}
 }
